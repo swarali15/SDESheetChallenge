0026ee844155bc2d70874be367156bc6d6193c0f
Swarali Purandare <swarali.purandare18@vit.edu>
2022-06-07 21:57:04 +0530

    Day2

---
+++ Sort0s1s.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+void sort012(int *nums, int n)
+{
+   //   Write your code here
+           int low=0 , mid=0, high= n-1;
+        
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1) mid++;
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
+}
